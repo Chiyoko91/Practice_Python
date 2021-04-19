9a980340ef0ccdac4d3c69f33c57a13c7ce8e8ae
chiyoko <hanamaruc77@gmail.com>
2021-04-19 17:37:19 +0900

    first commit

---
+++ chap2.py
@@ -0,0 +1,22 @@
+"""
+P y t h o n
+"""
+#test comment
+#I/O, operation test
+print("Say Hello!")
+print("おはよう") 
+print(15 / 60)
+print(3 + 2 * 2)
+print((3 + 2) * 2)
+print(2 / (60 / 30))
+print("15分は" + str(15 / 60) + "時間です")
+print(3 + 0.5)
+print(int ("3") + 3) #fit to int
+print("3" + str(3)) #fit to str
+minutes = 15
+print(str(minutes) + "分は" + str(minutes / 60) + "時間です")
+parrot = input("any input please:")
+print(parrot)
+
+minutes = int (input( "求めたい分を入力してください:") )
+print(str(minutes) + "分は" + str(minutes / 60) + "時間です")
