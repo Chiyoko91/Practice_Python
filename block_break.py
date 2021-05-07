10fe9ffc8376965c686e621a16c90acf8e20a82c
chiyoko <hanamaruc77@gmail.com>
2021-05-08 02:51:28 +0900

    起動するたびにブロックの色をランダムに変更

--- block_break.py
+++ block_break.py
@@ -1,5 +1,6 @@
 #ゲーム本体
 import tkinter as tk
+import random
 import sys
 
 #基本設定
@@ -15,14 +16,14 @@ class Window():
         self.center_x = self.width/2 #センター(x)
         self.center_y = self.height/2 #センター(y)
         self.tick = 20 #画面更新秒数(ms)
-        self.cv = tk.Canvas(root, width = self.width, height = self.height) #キャンバス生成
-        self.cv.pack()
 
     def resizeEvent(self,event): #ウィンドウサイズの変更
         size_x = tk.Tk().winfo_width() #変更後のウィンドウサイズ(x)
         size_y = tk.Tk().winfo_height() #変更後のウィンドウサイズ(y)
 
 win = Window() #ウィンドウのインスタンス生成
+cv = tk.Canvas(root, width = win.width, height = win.height) #キャンバス生成
+cv.pack()
 
 #ボール設定
 class Ball:
@@ -36,8 +37,8 @@ class Ball:
         self.color = "red" #ボールの色
     
     def draw(self): #描画
-        win.cv.create_oval(self.x-self.w, self.y-self.w, self.x+self.w, self.y+self.w, fill = self.color, tag = "ball")
-        win.cv.pack()
+        cv.create_oval(self.x-self.w, self.y-self.w, self.x+self.w, self.y+self.w, fill = self.color, tag = "ball")
+        cv.pack()
 
     def move(self): #動作
         #移動
@@ -54,7 +55,7 @@ class Ball:
             self.dy *= -1
 
     def delete(self):
-        win.cv.delete("ball")
+        cv.delete("ball")
 ball = Ball() #ボールのインスタンス生成
 
 #パドル設定
@@ -65,22 +66,22 @@ class Paddle:
         self.y = win.height - 30 #パドルの初期位置(x座標)
         self.wx = 45 #パドルの幅(x座標)
         self.wy = 8 #パドルの幅(y座標)
-        self.dx = 10 #パドルの移動量(y成分の変化なし)
-        self.color = "green" #パドルの色
+        self.dx = 15 #パドルの移動量(y成分の変化なし)
+        self.color = "blue" #パドルの色
     
     def draw(self): #描画
-        win.cv.create_rectangle(self.x-self.wx,self.y-self.wy,self.x+self.wx,self.y+self.wy, fill = self.color, tag = "paddle")
+        cv.create_rectangle(self.x-self.wx,self.y-self.wy, self.x+self.wx,self.y+self.wy, fill=self.color, tag="paddle")
 
     def right(self,event): #右移動
-        win.cv.delete("paddle")
+        cv.delete("paddle")
         self.x += self.dx
         self.draw()
     def left(self,event): #左移動
-        win.cv.delete("paddle")
+        cv.delete("paddle")
         self.x -= self.dx
         self.draw()
 
-    def move(self): #移動キー設定
+    def move(self): #移動キーバインド
         root.bind("<Right>",self.right)
         root.bind("<Left>",self.left)
 paddle = Paddle() #パドルのインスタンス生成
@@ -89,26 +90,32 @@ paddle = Paddle() #パドルのインスタンス生成
 class Block:
     w_x =60 #ブロックの幅(x座標)
     w_y = 30 #ブロックの幅(y座標)
-    global dy, score
+    #global dy, score
 
-    #ブロックのスイッチ 1:ON,0:OFF
+    #当たったかどうかの情報 1:まだ,0:当たった
     block_list =[[1,1,1,1,1,1,1,1,1,1],
+                 [1,1,1,1,1,1,1,1,1,1],
+                 [1,1,1,1,1,1,1,1,1,1],
                  [1,1,1,1,1,1,1,1,1,1],
                  [1,1,1,1,1,1,1,1,1,1]]
     def draw(self): #描画
-        for i in range(10):
-            for j in range(3):
-                    win.cv.create_rectangle(i*self.w_x, j*self.w_y, (i+1)*self.w_x, (j+1)*self.w_y, fill = "orange", tag = "block"+str(j)+str(i))
+        for j in range(5):
+            color_r = random.randint(0,15)
+            color_g = random.randint(0,15)
+            color_b = random.randint(0,15)
+            cord = '#' + hex(color_r)[2:] + hex(color_g)[2:] + hex(color_b)[2:]
+            for i in range(10):
+                    cv.create_rectangle(i*self.w_x, j*self.w_y, (i+1)*self.w_x, (j+1)*self.w_y, fill = cord, tag = "block"+str(j)+str(i))
 
     def reflect(self): #ボールの反射
         for i in range(10):
-            for j in range(3):
+            for j in range(5):
                 #ボールが上から反射
                 if (ball.y-ball.w < (j+1)*self.w_y #ボールがブロックより下にある
                     and i*self.w_x < ball.x < (i+1)*self.w_x #ボールがブロックの左右に挟まれている
                     and self.block_list[j][i] == 1): #スイッチが1
                         ball.dy *= -1 #反射
-                        win.cv.delete("block"+str(j)+str(i)) #ブロックを消す
+                        cv.delete("block"+str(j)+str(i)) #ブロックを消す
                         self.block_list[j][i] = 0 #スイッチを切る
                         score.score += 1 #加点
                         score.delete() #旧スコア削除
@@ -119,42 +126,35 @@ block = Block() #ブロックのインスタンス生成
 class Score():
     score = 0 #初期値
     def draw(self):
-        win.cv.create_text(win.width - 50, 470, text = "Score : " +str(self.score), font = ('FixedSys', 16), tag = "score")
+        cv.create_text(win.width - 50, 470, text = "Score : " +str(self.score), font = ('FixedSys', 16), tag = "score")
     def delete(self):
-        win.cv.delete("score")
+        cv.delete("score")
 score = Score() #スコアのインスタンス生成
 
 #ゲームオーバー
 def gameover():
-    global w, dx, dy
+    #global w, dx, dy
     if ball.y + ball.w > win.height :
-        win.cv.delete("paddle")
-        win.cv.delete("ball")
-        win.cv.create_text(win.center_x, win.center_y, text = 'GAME OVER', font = ('FixedSys', 90))
+        cv.delete("paddle")
+        cv.delete("ball")
+        cv.create_text(win.center_x, win.center_y, text = 'GAME OVER', font = ('FixedSys', 90))
         ball.w = 0
         ball.dx = 0
         ball.dy = 0
 
 #クリア
 def gameclear():
-    global w, dx, dy
+    #global w, dx, dy
     if score.score == 30 :
-        win.cv.delete("paddle")
-        win.cv.delete("ball")
-        win.cv.create_text(win.center_x, win.center_y, text = "GAME CLEAR", font = ('FixedSys', 90))
+        cv.delete("paddle")
+        cv.delete("ball")
+        cv.create_text(win.center_x, win.center_y, text = "GAME CLEAR", font = ('FixedSys', 90))
         ball.w = 0
         ball.dx = 0
         ball.dy = 0
 
-#初期描画
-ball.draw() #ボール
-paddle.draw() #パドル
-block.draw() #ブロック
-score.draw() #スコア
-
-#メイン処理
 def gameloop():
-    #block.resizeEvent() #ウィンドウサイズの変更に応じた処理
+    #Window.resizeEvent() #ウィンドウサイズの変更に応じた処理
     ball.delete() #ボール消去
     ball.move() #ボールを動かす
     paddle.move() #パドルを動かす
@@ -163,7 +163,30 @@ def gameloop():
     gameover() #ゲームオーバー表示
     gameclear() #クリア表示
     root.after(win.tick, gameloop) #最初に戻る
+'''
+#メイン処理
+def driver():
+    cv.create_text(win.center_x, win.center_y-40, text = 'ブロック崩し', font = (u'MS　ゴシック', 90))
+    cv.create_text(win.center_x, win.center_y+110, text = 'Play (Press "P")', font = (u'MS　ゴシック', 30))
+    cv.create_text(win.center_x, win.center_y+140, text = 'Setting (Press "S")', font = (u'MS　ゴシック', 30))
+
+    def play(self,event): #ゲーム
+
+
+    #def setting(self,event): #設定
+
+
+    def keys(self): #キーバインド
+        root.bind("<KeyPress>", self.play())
+        root.bind("<Key-s>", self.setting())
+'''
+#driver() #スタート画面
+#root.after(win.tick, driver) #最初に戻る
 
 #実行部
-gameloop()
+block.draw() #ブロック初期描画
+ball.draw() #ボール初期描画
+paddle.draw() #パドル初期描画
+score.draw() #スコア初期描画
+gameloop() #ゲーム中
 root.mainloop()
